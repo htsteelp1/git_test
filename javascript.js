6ff0106665a4dde074899355d32ba5fd6ca414cd
htsteelp1 <htsteelp1@gmail.com>
2026-03-29 17:04:40 -0400

    Completed Baisc function exercise

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function add7(num) {
+    return num+7;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function capitalize(input) {
+    input = input.toLowerCase();
+    return input.replace(input.at(0), input.at(0).toUpperCase());
+}
+function lastLetter(input) {
+    return input.at(-1);
+}
